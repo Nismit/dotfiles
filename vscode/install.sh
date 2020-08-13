e6c63fa94d5a03d7141cdf28c177c002cb93890c
Nismit <nismit.dev@gmail.com>
2020-08-12 22:42:13 -0700

    Update vscode installer

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,9 +1,11 @@
 # Install VSCode Extensions
-# code --install-extension coenraads.bracket-pair-colorizer-2
-# code --install-extension editorconfig.editorconfig
-# code --install-extension dbaeumer.vscode-eslint
-# code --install-extension oderwat.indent-rainbow
-# code --install-extension slevesque.shader
+vscode_install_extensions() {
+    code --install-extension coenraads.bracket-pair-colorizer-2
+    code --install-extension editorconfig.editorconfig
+    code --install-extension dbaeumer.vscode-eslint
+    code --install-extension oderwat.indent-rainbow
+    code --install-extension slevesque.shader
+}
 
 # Symbolic Link
 DOTPATH=~/.dotfiles/vscode; export DOTPATH
@@ -16,20 +18,17 @@ vscode_config_install() {
         [[ "$f" == "$VSCODE_PATH/snippets" ]] && continue
         [[ "$f" == "$VSCODE_PATH/workspaceStorage" ]] && continue
 
-        if [ ! ${f##*.} = "bak" ]; then
-            echo ${f##*.}
-            # echo "Make Backup..."
-            # command mv "$f" "$f.bak"
+        if [ ${f##*.} = "json" ]; then
+            echo "Make Backup..."
+            command mv "$f" "$f.bak"
         fi
-        # command ln -snf "$DOTPATH/`basename "$f"`" "$VSCODE_PATH/"
-
-        
+        command ln -snf "$DOTPATH/`basename "$f"`" "$VSCODE_PATH/"
     done
 }
 
 if [ ! -L "$VSCODE_PATH/keybindings.json" ]; then
     vscode_config_install
+    vscode_install_extensions
 else
     echo "Settings Already Linked!"
-    vscode_config_install
 fi

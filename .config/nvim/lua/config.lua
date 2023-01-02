4765ba5df4e71899c919ad806d5971eb85f18132
Nismit <nismit.dev@gmail.com>
2023-01-02 15:00:43 -0800

    fix color scheme

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -11,6 +11,7 @@ vim.opt.expandtab = true
 vim.opt.mouse = "a"
 -- Mac default terminal does not support 24bit colors
 -- vim.opt.termguicolors = true
+vim.cmd [[ colorscheme iceberg ]]
 
 --[[
 vim.api.nvim_create_autocmd({'TermOpen'},{
@@ -32,7 +33,20 @@ vim.api.nvim_create_user_command(
 )
 ]]
 
+-- Keymaps
+
+-- Terminal
 vim.api.nvim_set_keymap('t', '<ESC>', [[<C-\><C-n>]], { noremap = true })
+
+-- Screen
+vim.api.nvim_set_keymap('n', '<leader>sj', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>sk', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ss', '<C-u>sp<CR><C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>sv', '<C-u>vs<CR><C-w>i', { noremap = true })
+
+-- Plugins
 vim.api.nvim_set_keymap('n', '<leader>j', ':ToggleTerm<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true })
+

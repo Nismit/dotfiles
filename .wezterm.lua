6aab851da1c7d8f7f54cc4b5bf265ec84d427703
Nismit <nismit.dev@gmail.com>
2023-01-04 10:56:20 -0800

    Add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+return {
+  use_ime = true, -- Enable Japanese IME
+  font_size = 14.0,
+  window_background_opacity = 0.8,
+  color_scheme = 'iceberg-dark',
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+  initial_rows = 35,
+  initial_cols = 140,
+}
